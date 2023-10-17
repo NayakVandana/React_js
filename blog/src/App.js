7955b623b70514998e7febec9b3eb6abcffd28df
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-17 06:43:29 +0530

    props pass

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,28 +1,15 @@
-import React,{Component} from 'react'
 import './App.css';
-class  App extends Component {
+import Student from './Student'
+function App() {
 
-  constructor()
-  {
-    super();
-    this.state={
-      data:1
-    }
-  }
-  apple()
-  {
-    this.setState({data:this.state.data+1})
-  }
-  render()
-  {
-    return (
-      <div className="App">
-       <h1>{this.state.data}</h1>
-       <button onClick={()=>this.apple()}>Update Data</button>
-      </div>
-    );
-  }
- 
+  return (
+    <div className="App">
+     <h1>Props in React :)</h1>
+     <Student name={"anil"}  email="anil@test.com" other={{address:"delhi",mobile:"000"}}/>
+     <Student name={"anil"}  email="anil@test.com" other={{address:"delhi",mobile:"000"}}/>
+     <Student name={"anil"}  email="anil@test.com" other={{address:"delhi",mobile:"000"}}/>
+    </div>
+  );
 }
 
 export default App;
