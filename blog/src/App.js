3f1d78f12b53b69bc8a5119bba933acbea029b19
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-17 05:40:43 +0530

    class component

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import User from './User'
 function App() {
+
+  function Apple()
+  {
+    return(<div>Apple component</div>)
+  }
   return (
     <div className="App">
      <h1>Hello World !</h1>
+     <User />
+     <Apple />
     </div>
   );
 }
