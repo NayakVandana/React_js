01183a1734aa913d17cb151d12ad8db2d6f4d57d
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-17 06:28:16 +0530

    state_with_class_component

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,20 +1,28 @@
-import {useState} from 'react'
+import React,{Component} from 'react'
 import './App.css';
+class  App extends Component {
 
-function App() {
-  const [data,setData]=useState(0)
-  function updateData()
+  constructor()
   {
-    setData(data+1)
-   
+    super();
+    this.state={
+      data:1
+    }
   }
-  console.warn("______");
-  return (
-    <div className="App">
-     <h1>{data}</h1>
-     <button onClick={updateData}>Update Data</button>
-    </div>
-  );
+  apple()
+  {
+    this.setState({data:this.state.data+1})
+  }
+  render()
+  {
+    return (
+      <div className="App">
+       <h1>{this.state.data}</h1>
+       <button onClick={()=>this.apple()}>Update Data</button>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
