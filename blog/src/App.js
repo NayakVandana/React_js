58d00ac0f20624c91a5a6bf8f1d8f6a32ef4fb52
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-18 05:38:01 +0530

    hide show and toggle

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
-import React ,{useState}from 'react'
+import React,{useState} from 'react'
 function App() {
-const [data,setData]=useState(null)
-const [print,setPrint]=useState(false)
-
-  function getData(val)
-  {
-    console.warn(val.target.value)
-    setData(val.target.value)
-    setPrint(false)
-  }
+  const [show,setShow]=useState(true)
   return (
     <div className="App">
      {
-       print?
-       <h1> {data}</h1>
-       :null
+       show?<h1>Hello World !</h1>:null
      }
-    <input type="text" onChange={getData} />
-    <button onClick={()=>setPrint(true)} >Print Data</button>
+     {/* <button onClick={()=>setShow(true)} >Show</button>
+     <button onClick={()=>setShow(false)} >Hide</button> */}
+<button onClick={()=>setShow(!show)} >Toggle</button> 
     </div>
   );
 }
