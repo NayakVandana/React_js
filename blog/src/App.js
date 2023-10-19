93a92e8aa96b084b06c1744a40603f8a886d81d2
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-19 06:55:16 +0530

    componentDidUpdate

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,21 +5,32 @@ class App extends React.Component {
   {
     super();
     console.warn("constructor")
-    this.state={name:"anil"}
+    this.state={
+     count:0
+    }
   }
-  componentDidMount()
+  componentDidUpdate(preProps,preState,snapshot)
   {
-    console.warn("componentDidMount")
+    // console.warn("componentDidUpdate",preState)
 
+    console.warn("componentDidUpdate",preState.count,this.state.count,snapshot) //snapsshot tyare j call thase jyare getSnapshotBeforeUpdate() baki undefine aavse
+    // if(preState.count===this.state.count){
+    //   alert("data is alreday same")
+    // }
+    if(this.state.count<10){
+      this.setState({count:this.state.count+1})
+    }
+   
   }
   render()
   {
-    console.warn("render")
+    // console.warn("render")
 
     return (
       <div className="App">
-        <h1>Component Did Mount {this.state.name}</h1>
-        <button onClick={()=>this.setState({name:"Sidhu"})}>Update</button>
+        <h1>Component Did Mount {this.state.count}</h1>
+        {/* <button onClick={()=>this.setState({count:this.state.count+1})}>Update</button> */}
+        <button onClick={()=>this.setState({count:1})}>Update</button>
       </div>
     );
   }
