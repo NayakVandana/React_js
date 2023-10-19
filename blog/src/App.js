2f4b5f4da868ff7d20ea9642c85f8848d229df5c
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-19 05:56:02 +0530

    render life cycle method

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
 import React from 'react'
-class App extends React.Component{
-  constructor()
-  {
-    super();
-    this.state={
-      name:"Anil sidhu"
-    }
-    console.warn("constructor")
-  }
-  render()
-  {
-    console.warn("render")
-    return<div>
-      <h1>Hello World {this.state.name}</h1>
+import User from './User'
+function App() {
+const [name,setName]=React.useState("Anil")
+
+  return (
+    <div className="App">
+     <h1>Render Method in React</h1>
+     <User />
+     {/* <button onClick={()=>setName("Sidhu")}>Update Name</button> */}
     </div>
-  }
+  );
 }
+
 export default App;
--- blog/src/User.js
+++ blog/src/User.js
@@ -1,12 +1,20 @@
-function User(props) {
-    
-    return(
-        <div>
-            <h1>User Component</h1>
-            <button onClick={props.data}>Call Function</button>
-        </div>
-    )
+import React from 'react'
+class User extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "anil@test.com"
+        }
+    }
+    render() {
+        console.warn("Render method", this.state.email)
+        return (
+            <div>
+                <h1>User Component </h1>
+                <button onClick={() => this.setState({ email: "sidhu@test.com" })}>Update Email</button>
+            </div>
+        )
+    }
 }
-
 
 export default User;
