7be73a4eff137cf103842d60f0c11c5511cad244
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-20 06:55:48 +0530

    useEffect with condition

--- blog/src/User.js
+++ blog/src/User.js
@@ -1,20 +1,18 @@
-import React from 'react'
-class User extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            email: "anil@test.com"
-        }
-    }
-    render() {
-        console.warn("Render method", this.state.email)
-        return (
-            <div>
-                <h1>User Component </h1>
-                <button onClick={() => this.setState({ email: "sidhu@test.com" })}>Update Email</button>
-            </div>
-        )
-    }
+import './App.css';
+import React,{useEffect} from 'react'
+function User(props) {
+  useEffect(()=>{
+    console.warn("use effect is called")
+  },[props.count,props.data])
+  return (
+    <div className="App">
+      <h1>Data : {props.data}</h1>
+      <h1>Count : {props.count}</h1>
+      
+    
+
+    </div>
+  );
 }
 
 export default User;
