a431ee70cffdf4e28b92aad53ade77c8ff50aeb4
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-20 06:21:03 +0530

    Hooks(useState)

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,13 @@
 import './App.css';
-import React from 'react'
-import Student from './Student'
-class App extends React.Component {
-  constructor()
-  {
-    super();
-    this.state={
-      show:true
-    }
-  }
-  render() {
-    return (
-      <div className="App">
-        {
-          this.state.show?<Student />
-          :<h4>Component is removed</h4>
-        }
-        <button onClick={()=>this.setState({show:false})}> Toogle Student Component</button>
-      </div>
-    )
-  }
+import React,{useState}from 'react'
+function App() {
+  const [data,setData]=useState("Anil")
+  return (
+    <div className="App">
+      <h1>Hooks in React {data}</h1>
+      <button onClick={()=>setData("Peter")}>Update State</button>
+    </div>
+  );
 }
 
 export default App;
