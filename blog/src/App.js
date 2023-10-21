52876a555f3f8006fc3753e51e92a714decf5f3f
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-21 06:33:38 +0530

    fragment

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,29 +1,29 @@
 import './App.css';
 import React from 'react'
-import User from './User'
+import Cols from './Cols'
 function App() {
-  const users = [
-    {
-      name: 'Anil', email: 'anil@test.com', contact: "111"
-    },
-    {
-      name: 'Burce', email: 'bruce@test.com', contact: "222"
-    },
-    {
-      name: 'Peter', email: 'peter@test.com', contact: "333"
-    },
-    {
-      name: 'Sam', email: 'sam@test.com', contact: "444"
-    },
-  ]
   return (
-    <div className="App">
-      <h1>Reuse Component with List</h1>
+    <>
+      <h1>React Fragment</h1>
+     <table>
+       <tbody>
+         <tr>
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+
+         </tr>
+       </tbody>
+     </table>
+      </>
       
-      {
-        users.map((item)=><User  data={item} />)
-      }
-    </div>
   );
 }
 
