888015d6a619a11f9fdce54d4af8c6597bbd460a
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-21 06:47:29 +0530

    Send Data Child to Parent Component

--- blog/src/User.js
+++ blog/src/User.js
@@ -1,13 +1,12 @@
-function User(props) {
-
-  return (
-      <div>
-          <span>{props.data.name}</span>
-          <span> {props.data.email}</span>
-          <span> {props.data.contact}</span>
-      </div>
-
-  )
+function User(props)
+{
+    const name="Anil Sidhu"
+    return(
+        <div>
+            <h1>User Name is : </h1>
+            <button onClick={()=>props.getData(name)} >Click Me</button>
+        </div>
+    )
 }
 
 export default User;
