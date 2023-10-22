a87285f2f8fbbd9c799d38e4d2484a38ab1dd23f
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-22 07:17:35 +0530

    forwardRef parent to child

--- blog/src/User.js
+++ blog/src/User.js
@@ -1,12 +1,11 @@
-function User(props)
+import React,{forwardRef} from 'react'
+function User(props,ref)
 {
-    const name="Anil Sidhu"
     return(
         <div>
-            <h1>User Name is : </h1>
-            <button onClick={()=>props.getData(name)} >Click Me</button>
+            <input ref={ref} type="text" />
         </div>
     )
 }
 
-export default User;
+export default forwardRef(User);
