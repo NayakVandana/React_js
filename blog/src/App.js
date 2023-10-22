21f46260023945e00f2ba0f31bf826ade62b5cc0
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-22 06:50:40 +0530

    useMemo

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
-import React from 'react'
-import Counter from './Counter'
-class  App extends React.Component {
-  constructor()
-  {
-    super();
-    this.state={
-      count:1
+import React, { useState } from 'react'
+function App() {
+  const [count, setData] = useState(1)
+  const [item, setItem] = useState(20)
+
+  const newApple=React.useMemo(//use Memo appleTime badhij event par call thai jase ane stop karava particular event par use karava use thai 
+    function appleTime() {
+      console.warn("Hello")
+      return 100 * count;
     }
-  }
- render()
- {
+  ,[item])
   return (
     <div className="App">
-     <Counter count={this.state.count} />
-      <button 
-      onClick={()=>{this.setState({count:this.state.count+1})}}
-      >Update Count</button>
+      <h1>Hooks in React {count}</h1>
+      {newApple}
+      <button onClick={() => setData(count + 1)}>Update State</button>
+      <button onClick={() => setItem(item * 10)}>Update State</button>
+
     </div>
   );
- }
 }
 
 export default App;
