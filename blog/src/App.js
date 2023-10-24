21d2bb719fa6cffb0bf128171a8caa69c70bb602
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-24 07:04:06 +0530

    react-router-dom

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,35 +1,20 @@
-import './App.css';
-import React, { useRef, useState } from 'react'
+//npm i react-router-dom
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './component/Home'
+import About from './component/About'
 function App() {
   return (
     <div className="App">
-      <h1>HOC </h1>
-      <HOCRed cmp={Counter} />
-      <HOCGreen cmp={Counter} />
-      <HOCBlue cmp={Counter} />
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />              
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
-function HOCRed(props)
-{
-  return <h2 style={{backgroundColor:'red',width:100}}>Red<props.cmp /></h2>
-}
-function HOCGreen(props)
-{
-  return <h2 style={{backgroundColor:'green',width:100}}>Grren<props.cmp /></h2>
-}
-function HOCBlue(props)
-{
-  return <h2 style={{backgroundColor:'blue',width:100}}>blue <props.cmp /></h2>
-}
-function Counter()
-{
-  const [count,setCount]=useState(0)
-  return<div>
-    <h3>{count}</h3>
-    <button onClick={()=>setCount(count+1)}>Update</button>
-  </div>
 }
 
 export default App;
