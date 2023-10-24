d8a4f904bc379d3bd96328f00caf743adfa404f7
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-24 07:43:01 +0530

    Page404

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,11 @@
 //npm i react-router-dom
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import Home from './component/Home'
 import About from './component/About'
 import NavBar from "./component/NavBar";
+// import Page404 from "./component/Page404";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Routes>         
           <Route path="/" element={<Home />} />              
           <Route path="/about" element={<About />}/>
+          {/* <Route path="/*" element={<Page404 />}/> */}
+          <Route path="/*" element={<Navigate to="/" />}/>
         </Routes>
       </BrowserRouter>
     </div>
--- blog/src/component/NavBar.js
+++ blog/src/component/NavBar.js
@@ -1,10 +1,11 @@
-
+import { Link } from "react-router-dom"
 function NavBar() {
     return (
       <div>
         <ul>
-            <li> <a href="/">Home</a></li>
-            <li> <a href="/about">About</a></li>
+            <li><Link to="/"> Home</Link></li>
+            <li><Link to="/about"> About</Link></li>
+            <li><Link to="/contact"> Contact</Link></li>
         </ul>
       </div>
     )
