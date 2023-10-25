b8d6d73e8c384af9cc6d2d8357b6df15bae4992d
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-25 06:32:06 +0530

    Active Link

--- blog/src/component/NavBar.js
+++ blog/src/component/NavBar.js
@@ -3,9 +3,9 @@ function NavBar() {
     return (
       <div>
         <ul>
-            <li><NavLink className="nav-bar-link" style={{color:'red'}}  to="/"> Home</NavLink></li>
-            <li><NavLink className="nav-bar-link" to="/about"> About</NavLink></li>
-            <li><NavLink className="nav-bar-link" to="/contact"> Contact</NavLink></li>
+            <li><NavLink className="nav-bar-link" style={({isActive})=>{return{color:isActive?"skyblue":""}}}  to="/"> Home</NavLink></li>
+            <li><NavLink className="nav-bar-link" style={({isActive})=>{return{color:isActive?"skyblue":""}}} to="/about"> About</NavLink></li>
+            <li><NavLink className="nav-bar-link" style={({isActive})=>{return{color:isActive?"skyblue":""}}} to="/contact"> Contact</NavLink></li>
             
         </ul>
       </div>
