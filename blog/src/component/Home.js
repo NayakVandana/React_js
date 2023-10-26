63752242ff1118e276cda781e9e974f490282017
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-26 06:12:33 +0530

    useLocation Hooks

--- blog/src/component/Home.js
+++ blog/src/component/Home.js
@@ -1,18 +1,12 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 function Home() {
-  const Navigate = useNavigate();
-  const navToPage = (url) => {
-    Navigate(url);
-  };
+  const location =useLocation();
+  console.log(location)
   return (
     <div>
       <h1>Home Page</h1>
       <p>This is my Home Page</p>
-      <Link to="/about"> Go to about</Link>
-      <br />
-      <button onClick={() => navToPage("/about")}>Go to About</button>
-      <br />
-      <button onClick={() => navToPage("/filter")}>Go to Filter</button>
+      <Link to="/about"> Go to about</Link>      
     </div>
   );
 }
--- blog/src/component/User.js
+++ blog/src/component/User.js
@@ -1,9 +1,10 @@
-import { useParams } from "react-router-dom";
+import { useParams,useLocation} from "react-router-dom";
 
 function User()
 {const params = useParams();
     const {name}= params;
-    console.warn(name);
+    const location =useLocation();
+  console.log(location)
     return(
         <div>
           <h1>This is {name}'s page</h1>
