b78d93bff49be3c7edad42518019351d5d3f8563
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-30 06:26:11 +0530

    update and put method

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,8 +5,7 @@ function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
-  const [userId, setUserId] = useState(null);
-
+  const [userId,setUserId]=useState(null)
 
   useEffect(() => {
     getUsers();
@@ -20,7 +19,6 @@ function App() {
         setMobile(resp[0].mobile)
         setEmail(resp[0].email)
         setUserId(resp[0].id)
-
       })
     })
   }
@@ -35,24 +33,23 @@ function App() {
       })
     })
   }
-  function selectUser(id) {
-    
-    let item = users[id-1]
-    console.warn("apple",item,users,id)
+  function selectUser(id)
+  {
+    let item=users[id-1];
     setName(item.name)
-    setMobile(item.mobile)
-    setEmail(item.email)
-    setUserId(item.id)
-
-    
+        setEmail(item.email)
+        setMobile(item.mobile);
+        setUserId(item.id)
   }
-  function updateUser() {
-    let item={name,email,mobile}
+  function updateUser()
+  {
+    let item={name,mobile,email}
+    console.warn("item",item)
     fetch(`http://localhost:4000/todo/${userId}`, {
       method: 'PUT',
       headers:{
-      'Accept':'application/json',
-      'Content-Type':'application/json'
+        'Accept':'application/json',
+        'Content-Type':'application/json'
       },
       body:JSON.stringify(item)
     }).then((result) => {
@@ -64,7 +61,7 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>Delete User With API </h1>
+      <h1>Update User Data With API </h1>
       <table border="1" style={{ float: 'left' }}>
         <tbody>
           <tr>
@@ -90,10 +87,10 @@ function App() {
         </tbody>
       </table>
       <div>
-        <input type="text" value={name} /> <br /><br />
-        <input type="text" value={email} /> <br /><br />
-        <input type="text" value={mobile} onChange={(e) => { setMobile(e.target.value) }} /> <br /><br />
-        <button onClick={updateUser} >Update User</button>
+      <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} /> <br /><br />
+        <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} /> <br /><br />
+        <input type="text" value={mobile}  onChange={(e)=>{setMobile(e.target.value)}} /> <br /><br />
+        <button onClick={updateUser} >Update User</button>  
       </div>
     </div>
   );
