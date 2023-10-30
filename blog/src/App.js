40e6b7ddbdd924ad12da925f0b79b96531fb8e46
Vandana Nayak <vandunayak4000@gmail.com>
2023-10-30 07:00:59 +0530

    State with Object

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import React from 'react'
-import Child from './Child'
+
 function App() {
-  const [count,setCount]=React.useState(0)
+  const [data,setData]=React.useState({name:"anil",age:27})
   return (
     <div className="App">
-      <Child count={count} />
-      <button onClick={()=>{setCount( Math.floor(Math.random()*10) )}} >Update Counter</button>
+      <h1>State object with hook</h1>
+      <input type='text' placeholder='enter name' value={data.name}
+       onChange={(e)=>{setData({...data,name:e.target.value})}}/>
+        <input type='text' placeholder='enter age' value={data.age}
+       onChange={(e)=>{setData({...data,age:e.target.value})}}/>
+       <h1>{data.name}</h1>
+       <h1>{data.age}</h1>
     </div>
   );
 }
